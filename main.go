f3ea06aa6c07cbceffa86546238242cb2963c39f
tsubasaogawa <hurikake@gmail.com>
2023-02-16 00:23:34 +0900

    Insert version by GitHub Action

--- main.go
+++ main.go
@@ -22,9 +22,8 @@ type ModuleBlock struct {
 	Variables []tfconfig.Variable
 }
 
-const VERSION string = "0.0.6"
-
 var (
+	version string
 	//go:embed module_block.tmpl
 	tmpl string
 	//go:embed module_block_vscode.tmpl
@@ -116,7 +115,7 @@ func main() {
 	flag.Parse()
 
 	if *v {
-		fmt.Println(VERSION)
+		fmt.Println(version)
 		os.Exit(0)
 	}
 
